0c40fbfd8b0d231f5d0ee66c113b34eebe0a1476
jwujesq8 <zhukovskaja.elena@gmail.com>
2025-03-08 12:27:38 +0300

    handled exception

--- src/main/java/com/Atipera_github_task/exception/GitHubExceptionHandler.java
+++ src/main/java/com/Atipera_github_task/exception/GitHubExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.Atipera_github_task.exception;
 
 import io.smallrye.mutiny.Uni;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +12,9 @@ import java.util.Map;
 public class GitHubExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public Uni<Map<String, Object>> handleUserNotFound(UserNotFoundException e) {
-        return Uni.createFrom().item(Map.of("status", 404, "message", e.getMessage()));
+    public ResponseEntity<Map<String, Object>> handleUserNotFound(UserNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("status", 404, "message", e.getMessage()));
     }
 }
--- src/main/java/com/Atipera_github_task/service/GitHubServiceImpl.java
+++ src/main/java/com/Atipera_github_task/service/GitHubServiceImpl.java
@@ -7,6 +7,7 @@ import com.Atipera_github_task.exception.UserNotFoundException;
 import io.smallrye.mutiny.Uni;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
@@ -22,13 +23,14 @@ public class GitHubServiceImpl implements GitHubService {
     private final WebClient webClient = WebClient.builder().baseUrl("https://api.github.com").build();;
 
     public Uni<List<ReposInfoResponseDto>> listNonForkReposByUsername(String username) {
+
         List<RepositoryDto> repositoryDtoList =  webClient.get()
                 .uri(LIST_REPOS_BY_USERNAME_PATH, username)
                 .retrieve()
                 .bodyToFlux(RepositoryDto.class)
                 .filter(repo -> !repo.isFork())
                 .collectList()
-                .onErrorResume(WebClientResponseException.NotFound.class, e -> Mono.error(new UserNotFoundException(username)))
+                .onErrorResume(e -> Mono.error(new UserNotFoundException(username)))
                 .block();
 
         if(repositoryDtoList == null) {return Uni.createFrom().item(new ArrayList<>());}
